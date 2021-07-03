10cf5da2a11b5335b5a13af4841eeef0e711fc9d
gouqie722 <3147592685@qq.com>
2021-07-03 14:25:19 +0800

    1

---
+++ index1.js
@@ -0,0 +1,17 @@
+  // 在GitHub上创建储存库  复制
+  // git clone git@github.com:15016788356/JavaScriptDesignPatterns.git
+  // git remote -v 查看远程分支
+  // git add 'fileName'
+  // git add . 添加所有文件
+  // git commit describe 对当前提交进行描述
+  // git state 查看当前提交的状态
+
+
+  // 什么是设计模式, 原则
+
+
+
+  // 设计模式的六大原则之六
+  // 接口分离原则
+  // 把大接口拆分成小接口，不能一个接口实现增删改查
+  // 比如说我写作业， 要细分成，写数学，语文，英语等作业，同时降低了耦合度
